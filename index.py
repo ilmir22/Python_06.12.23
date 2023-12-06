43338d563033e9179685251661602562db46276b
ilmir22 <garifullin-ilmir@bk.ru>
2023-12-06 20:57:23 +0400

    Create index.py

---
+++ index.py
@@ -0,0 +1,39 @@
+# print (2+2)
+# print (2 ** 2** 3)
+
+# my_number = 2
+# print(my_number)
+# my_number = str(my_number)
+# print("Python version:", my_number)
+# print("Python version:" + my_number)
+# var = 1
+# print(var)
+# var=var+4
+# print(var)
+# user_word = int(input("Введите число:"))
+# print(user_word+3)
+# print(type(user_word))
+
+#############################
+
+# Третье задание 
+# first_num=int(input("enter the first number"))
+# second_num=int(input("enter the second number"))
+# print(f"сумма чисел:{first_num+second_num}")
+
+# Четвертое задание
+# first_number=int(input("enter the first number:"))
+# procent=int(input("enter the second procent:"))
+# var=first_number * procent/100
+# print(var) 
+# Пользователь вводит с клавиатуры два числа. Первое
+# число — это значение, второе число процент, который
+# необходимо посчитать. Например, мы ввели с клавиатуры
+# 50 и 10. Требуется вывести на экран 10 процентов от 50.
+# Результат: 5
+#
+# Пятое задание
+# width=int(input("Введите ширину:"))
+# height=int(input("Введите высоту:"))
+# var=width*height
+# print(var)
